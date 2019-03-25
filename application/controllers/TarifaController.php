3a8c3047c2011878446c4d0ddd820b445d7274e3
Leonardo Severo <leopedroso45@gmail.com>
2019-03-25 18:25:06 -0300

    Banco de dados e controllers para trabalhar...

---
+++ application/controllers/TarifaController.php
@@ -0,0 +1,21 @@
+﻿<?php
+
+class TarifaController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+       
+    }
+
+    public function cadastrarAction()
+    {
+   
+    }
+
+}
