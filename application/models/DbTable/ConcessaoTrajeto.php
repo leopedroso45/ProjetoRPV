0cc605677d627444aa1c08f4b7d00697644a4903
Vagner Quincozes <vequincozes@gmail.com>
2019-03-29 14:24:49 -0300

    Cadastrando concessao de trajeto no banco de dados!

--- application/models/DbTable/ConcessaoTrajeto.php
+++ application/models/DbTable/ConcessaoTrajeto.php
@@ -13,6 +13,10 @@ class Application_Model_DbTable_ConcessaoTrajeto extends Zend_Db_Table_Abstract
         $concessaoTrajeto = $this->createRow();
         /*@var $concessaoTrajeto Application_Model_ConcessaoTrajeto*/
         $concessaoTrajeto->setData_inicio($dados['dataInicio']);
+        $concessaoTrajeto->setData_fim($dados['dataFim']);
+        $concessaoTrajeto->setcodConcessao($dados['codConcessao']);
+        $concessaoTrajeto->setDoc_autorizacao($dados['docAutorizacao']);
+        $concessaoTrajeto->setStatus_concessao($dados['statusConcessao']);        
         
         return $concessaoTrajeto->save();
     }
