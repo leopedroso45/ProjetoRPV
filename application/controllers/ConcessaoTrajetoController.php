5c73f7d3451c1c64c730e09abbd5dc1643fa5721
mauriciounipampa <mauriciosescobar@gmail.com>
2019-03-28 21:34:20 -0300

    commit pro Vagner

--- application/controllers/ConcessaoTrajetoController.php
+++ application/controllers/ConcessaoTrajetoController.php
@@ -10,66 +10,15 @@ class ConcessaoTrajetoController extends Zend_Controller_Action
 
     public function indexAction()
     {
-			if ($this->getRequest()->isPost()) {
-			$dados = $this->getRequest()->getParams(); 
-//var_dump($dados);die();			
-			$dbTableConcessao_Trajeto = new Application_Model_DbTable_Concessao_Trajeto();
-			$dbTableConcessao_Trajeto->cadastrarConcessao($dados);            
-	}
-    }
-
-    public function cadastrarAction()
-    {
-
-
-
-    }
-
-    public function editarAction()
-    {
-        $this->view->headScript()->appendFile($this->view->baseUrl('bootstrap/js/janela-de-dialogo.js'));
+//        $dbTableUsuario = new Application_Model_DbTable_Usuario();
+//        $lista = $dbTableUsuario->listarTodosUsuario();
+//        $this->view->listaDosUsuarios = $lista;
         
-        $this->view->message = '';
-        $id = $this->getRequest()->getParam('id');
-        $formulario = new Application_Form_UsuarioEditar();
-        $dbTableUsuario = new Application_Model_DbTable_Usuario();
-        $usuario = $dbTableUsuario->getUsuarioPorId($id);
-        $formulario->populate($usuario->toArray());
         if ($this->getRequest()->isPost()) {
-            if ($formulario->isValid($_POST)) {
                 $dados = $this->getRequest()->getParams();
-                $cod = $dbTableUsuario->editarUsuario($id, $dados);
-                $upload = new Zend_File_Transfer();
-                $foto = $upload->getFileInfo();
-                @$foto['cod_usuario'] = $cod;
-                $uploadfile = $_FILES['foto']['name'];
-                if ($uploadfile != '') {
-                    $modelImagem = new Application_Model_Imagem();
-                    $modelImagem->setCaminhoTemp($foto['foto']['tmp_name']);
-                    $modelImagem->setIdentidade(Zend_Auth::getInstance()->getIdentity());
-                    $caminho = $modelImagem->moverFotoDaUsuarioCadastrada($foto);
-                    $dbTableUsuario->alterarCaminhoFoto($caminho, $cod);
-                }
-                if ($cod != NULL) {
-                    $this->view->message = "Os dados foram alterados!";
-                } else {
-                    $this->view->message = "Não foi possível efetuar o cadastro!";
-                }
-            }
-        }
-        $this->view->formulario = $formulario;
-    }
+                $dbTableConcessaoTrajeto = new Application_Model_DbTable_ConcessaoTrajeto();
+                $dbTableConcessaoTrajeto->cadastrarConcessaoTrajeto($dados);
+        }}
 
-    public function excluirAction()
-    {
-        $id = $this->getRequest()->getParam('id');
-        $usuario = new Application_Model_DbTable_Usuario();
-        $numero = $usuario->excluirUsuario($id);
-        if ($numero > 0) {
-            $this->_helper->json(array('ok'));
-        } else {
-            $this->_helper->json(array('nok'));
-        }
-    }
 
 }
--- application/models/ConcessaoTrajeto.php
+++ application/models/ConcessaoTrajeto.php
@@ -2,50 +2,29 @@
 
 class Application_Model_ConcessaoTrajeto extends Zend_Db_Table_Row_Abstract
 {
-//    private $codConcessao;
-//    private $dataInicio;
-//    private $dataFim;
-//    private $docAutorizacao;
-//    private $statusConcessao;
     
-    public function getcodConcessao() {
-        return $this->codConcessao;
-    }
-    
-    public function getdataInicio() {
-        return $this->dataInicio;
-    }
+//        private $data_inicio;
+        
+        function getData_inicio()
+        {
+            return $this->data_inicio;
+        }
 
-    public function getdataFim() {
-        return $this->dataFim;
-    }
+        function setData_inicio($data_inicio)
+        {
+            $this->data_inicio = $data_inicio;
+        }
 
-    public function getdocAutorizacao() {
-        return $this->docAutorizacao;
-    }
 
-    public function getstatusConcessao() {
-        return $this->statusConcessao;
-    }
+        
 
-    public function setcodConcessao($codConcessao) {
-        $this->codConcessao = $codConcessao;
-    }
+
+
     
-    public function setdataInicio($dataInicio) {
-        $this->dataInicio = $dataInicio;
-    }
+
     
-    public function setdataFim($dataFim) {
-        $this->dataFim = $dataFim;
-    }
-    
-    public function setdocAutorizacao($docAutorizacao) {
-        $this->docAutorizacao = $docAutorizacao;
-    }
-    
-    public function setstatusConcessao($statusConcessao) {
-        $this->statusConcessao = $statusConcessao;
-    }
-    
+
+
+   
+
 }
