213fce29e4ab2ba3e2b6ae4f3dfdafb70c5378e4
Vagner Quincozes <vequincozes@gmail.com>
2019-04-03 22:38:38 -0300

    botão toggle (onn / off) no concessaoTrajetos

---
+++ public/dist/js/concessaoTrajeto.js
@@ -0,0 +1,18 @@
+
+
+    var estado = document.getElementById('estado');
+    $('#onoff1').on('change', function() {
+        var el = this;
+        estado.innerHTML = el.checked ? 'ligado' : 'desligado';
+
+        // aqui podes juntar a lógica do ajax
+        $.ajax({
+            url: "some.php",
+            data: {
+                estado: this.checked
+            }
+        }).done(function(msg) {
+            if (msg == 'failed') return el.checked = !el.checked; // caso o servidor retorne "failed" mudar o estado do botão
+            else alert("Info gravada: " + msg);
+        });
+    });
