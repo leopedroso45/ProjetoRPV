3202a1af2f239d2577ffb6073034cf78bc6b76ed
Vagner Quincozes <vequincozes@gmail.com>
2019-04-03 22:09:31 -0300

    modificações

--- application/controllers/ConcessaoTrajetoController.php
+++ application/controllers/ConcessaoTrajetoController.php
@@ -5,7 +5,7 @@ class ConcessaoTrajetoController extends Zend_Controller_Action
 
     public function init()
     {
-        /* Initialize action controller here */
+        $this->_helper->layout->setLayout("layout_adminlocal");
     }
 
     public function indexAction()
---
+++ application/models/DbTable/CategoriaPassageiro.php
@@ -0,0 +1,28 @@
+<?php
+
+class Application_Model_DbTable_ConcessaoTrajeto extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'concessaotrajeto';
+    protected $_rowClass = "Application_Model_ConcessaoTrajeto";
+    
+    public function cadastrarConcessaoTrajeto($dados)
+    {
+        
+//        var_dump($dados);die();
+        $concessaoTrajeto = $this->createRow();
+        /*@var $concessaoTrajeto Application_Model_ConcessaoTrajeto*/
+        $concessaoTrajeto->setData_inicio($dados['dataInicio']);
+        $concessaoTrajeto->setData_fim($dados['dataFim']);
+        $concessaoTrajeto->setDoc_autorizacao($dados['docAutorizacao']);
+        $concessaoTrajeto->setStatus_concessao($dados['statusConcessao']);        
+        
+        return $concessaoTrajeto->save();
+    }
+
+    public function listarTodasConcessoes(){
+       return $this->fetchAll();
+    }
+   
+
+}
