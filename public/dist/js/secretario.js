20163881ba2cc66c07feec9bc863df553f8cb2b1
mauriciounipampa <mauriciosescobar@gmail.com>
2019-04-07 21:36:34 -0300

    att 07/04

--- public/dist/js/secretario.js
+++ public/dist/js/secretario.js
@@ -1,9 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 $('#placa').mask('AAA-9999');
 $('#ano').mask('9999');
 $('#km').mask('99999999');
@@ -31,4 +25,121 @@ $(document).on('click', '.clickable', function (e) {
 //        $this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
 
     }
-})
+});
+
+//var auto_refresh = setInterval (function () {
+//    $.ajax({
+//        url: baseUrl + 'onibus-urbano/index',
+//        success: function (response) {
+//            $('.dataTable').html(response);
+//        }
+//    });
+//}, 3000);
+
+//     setInterval(function(){
+//            $(".dataTable").load()
+//        }, 1000);
+
+$(".cadastrar-onibus-urbano").click(function ()
+{
+    var placa = $("#placa").val();
+    var ano = $("#ano").val();
+    var situacao = $("#situacao").val();
+    var numero_assentos = $("#numero_assentos").val();
+    var numero_passageiros = $("#numero_passageiros").val();
+    var chassi = $("#chassi").val();
+    var km = $("#km").val();
+    var renavam = $("#renavam").val();
+    var cor = $("#cor").val();
+    var marca = $("#marca").val();
+    var modelo = $("#modelo").val();
+
+    if (placa.length < 7) {
+        $('.placa').removeClass('hidden');
+        $('.alert-success').addClass('hidden');
+
+    } else {
+        $.ajax({
+            type: "POST",
+            url: baseUrl + 'onibus-urbano/index',
+            async: false,
+            data: {placa: placa, ano: ano, situacao: situacao,
+                numero_assentos: numero_assentos, numero_passageiros: numero_passageiros,
+                chassi: chassi, km: km, renavam: renavam, cor: cor, marca: marca,
+                modelo: modelo
+            },
+            success: function () {
+                $(".alert-success").removeAttr("style");
+            },
+            error: function () {
+                alert("error");
+            }
+        });
+    }
+});
+//
+//$(".cadastrar-onibus-").click(function ()
+//{
+//    var placa = $("#placa").val();
+//    var ano = $("#ano").val();
+//    var situacao = $("#situacao").val();
+//    var numero_assentos = $("#numero_assentos").val();
+//    var numero_passageiros = $("#numero_passageiros").val();
+//    var chassi = $("#chassi").val();
+//    var km = $("#km").val();
+//    var email = $("#ano").val();
+//    var cpf = $("#situacao").val();
+//    var senha = $("#numero_assentos").val();
+//    var telefone = $("#numero_passageiros").val();
+//    var repetir_senha = $("#chassi").val();
+//
+//    if (nome.length < 10) {
+//        $('.nome').removeClass('hidden');
+//        $('.alert-success').addClass('hidden');
+//
+//    } else if (email.length < 10) {
+//        $('.nome').addClass('hidden');
+//        $('.email').removeClass('hidden');
+//        $('.alert-success').addClass('hidden');
+//
+//    } else if (cpf.length < 11) {
+//        $('.email').addClass('hidden');
+//        $('.cpf').removeClass('hidden');
+//        $('.alert-success').addClass('hidden');
+//
+//    } else if (telefone.length < 11) {
+//        $('.cpf').addClass('hidden');
+//        $('.telefone').removeClass('hidden');
+//        $('.alert-success').addClass('hidden');
+//
+//    } else if (senha.length < 8) {
+//        $('.telefone').addClass('hidden');
+//        $('.tamanho').removeClass('hidden');
+//        $('.alert-success').addClass('hidden');
+//
+//    } else if (senha !== repetir_senha) {
+//        $('.tamanho').addClass('hidden');
+//        $('.diferente').removeClass('hidden');
+//        $('.alert-success').addClass('hidden');
+//
+//    } else {
+//        $.ajax({
+//            type: "POST",
+//            url: baseUrl + 'onibus-urbano/index',
+//            async: false,
+//            data: {nome: nome, email: email, cpf: cpf,
+//                senha: senha, telefone: telefone
+//            },
+//            success: function () {
+//                $('.diferente').addClass('hidden');
+//                $('.alert-success').removeClass('hidden');
+//                document.getElementById("formulario").reset();
+//            },
+//            error: function () {
+//                alert("error");
+//            }
+//        });
+//    }
+//});
+
+
