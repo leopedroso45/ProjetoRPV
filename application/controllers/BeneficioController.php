bff63936a09a3d39eb1795f7418ff314d507bae9
Mauricio Escobar <mauriciosescobar@gmail.com>
2019-04-08 21:29:59 -0300

    layout redirects att

--- application/controllers/BeneficioController.php
+++ application/controllers/BeneficioController.php
@@ -5,14 +5,15 @@ class BeneficioController extends Zend_Controller_Action
 
     public function init()
     {
-		$this->_helper->layout->setLayout("layout_adminlocal");
+        $this->_helper->layout->setLayout('layout_admin_geral');
     }
 
     public function indexAction()
     {
-
+        
     }
-        public function deficienteAction()
+
+    public function deficienteAction()
     {
 
         $dbTableBeneficio = new Application_Model_DbTable_Beneficio();
@@ -20,32 +21,28 @@ class BeneficioController extends Zend_Controller_Action
         $this->view->listarBeneficios = $lista;
 
         //$id = $this->getRequest()->getParam('id');
-
-       // $dbTableBeneficio = new Application_Model_DbTable_Beneficio();
-       // $beneficio = $dbTableBeneficio->getBeneficioPorId($id);
+        // $dbTableBeneficio = new Application_Model_DbTable_Beneficio();
+        // $beneficio = $dbTableBeneficio->getBeneficioPorId($id);
 
         if ($this->getRequest()->isPost()) {
             $dados = $this->getRequest()->getParams();
             $dbTableBeneficio->editarBeneficios($id, $dados);
         }
         //$this->view->beneficio = $beneficio;
+    }
 
-
-    
-	
-	}    
-	
-	
     public function idosoAction()
     {
         $dbTableBeneficio = new Application_Model_DbTable_Beneficio();
         $lista = $dbTableBeneficio->listar();
         $this->view->listarBeneficios = $lista;
     }
-        public function estudanteAction()
+
+    public function estudanteAction()
     {
         $dbTableBeneficio = new Application_Model_DbTable_Beneficio();
         $lista = $dbTableBeneficio->listar();
         $this->view->listarBeneficios = $lista;
     }
+
 }
