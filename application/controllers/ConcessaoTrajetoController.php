c98f2b83c8b0aea3aa0e7e41636713d9e173beb0
Mauricio Escobar <mauriciosescobar@gmail.com>
2019-04-10 13:06:07 -0300

    integração pausada!

--- application/controllers/ConcessaoTrajetoController.php
+++ application/controllers/ConcessaoTrajetoController.php
@@ -10,9 +10,9 @@ class ConcessaoTrajetoController extends Zend_Controller_Action
 
     public function indexAction()
     {
-//        $dbTableUsuario = new Application_Model_DbTable_Usuario();
-//        $lista = $dbTableUsuario->listarTodosUsuario();
-//        $this->view->listaDosUsuarios = $lista;
+        $dbTableConcessaoTrajeto = new Application_Model_DbTable_ConcessaoTrajeto();
+        $lista = $dbTableConcessaoTrajeto->listarTodasConcessoes();
+        $this->view->listaDasConcessoes = $lista;
         
         
         if ($this->getRequest()->isPost()) {
--- application/models/DbTable/ConcessaoTrajeto.php
+++ application/models/DbTable/ConcessaoTrajeto.php
@@ -5,17 +5,21 @@ class Application_Model_DbTable_ConcessaoTrajeto extends Zend_Db_Table_Abstract
 
     protected $_name = 'concessaotrajeto';
     protected $_rowClass = "Application_Model_ConcessaoTrajeto";
-    
+
+    public function listarTodasConcessoes()
+    {
+        return $this->fetchAll();
+    }
+
     public function cadastrarConcessaoTrajeto($dados)
     {
-        
+
 //        var_dump($dados);die();
         $concessaoTrajeto = $this->createRow();
-        /*@var $concessaoTrajeto Application_Model_ConcessaoTrajeto*/
+        /* @var $concessaoTrajeto Application_Model_ConcessaoTrajeto */
         $concessaoTrajeto->setData_inicio($dados['dataInicio']);
-        
+
         return $concessaoTrajeto->save();
     }
-   
 
 }
