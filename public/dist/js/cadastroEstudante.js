727ebbbd8948f9b547ce561ac1b71698a939e1d1
Vagner Quincozes <vequincozes@gmail.com>
2019-04-27 19:04:47 -0300

    Validações nos campos (falta terminar o campo do checkbox)

--- public/dist/js/cadastroEstudante.js
+++ public/dist/js/cadastroEstudante.js
@@ -58,3 +58,74 @@ function SomenteNumero(e) {
             return false;
     }
 }
+
+function verify() {
+    var senha = document.getElementById("senha").value;
+    var confirmacao = document.getElementById("confirmarSenha").value;
+    if (senha == confirmacao) {
+        document.getElementById("mensagem").innerHTML = ""
+        document.getElementById('cadastro').disabled = false;
+    } else {
+        document.getElementById("mensagem").innerHTML = "Erro! As senhas nao correspodem."
+        document.getElementById('cadastro').disabled = true;
+    }
+}
+
+function minemaxlenght() {
+    var minimo = document.getElementById("senha").value.length;
+    var confirmar = document.getElementById("confirmarSenha").value.length;
+    if (minimo > 5) {
+        document.getElementById("mensagem").innerHTML = ""
+        document.getElementById('cadastro').disabled = false;
+    } else {
+        document.getElementById("mensagem").innerHTML = "Erro! Minímo 6 caracteres."
+        document.getElementById('cadastro').disabled = true;
+    }
+}
+
+function Ncompleto() {
+    var nome = document.getElementById("nomeCompleto").value.length;
+    if (nome < 10) {
+        document.getElementById("mensagem1").innerHTML = "Erro! Escreva o seu nome e sobrenome."
+        document.getElementById('cadastro').disabled = true;
+    } else {
+        document.getElementById("mensagem1").innerHTML = ""
+        document.getElementById('cadastro').disabled = false;
+    }
+}
+
+function validaUsuario() {
+    var usuario = document.getElementById("nome_usuario").value.length;
+    if (usuario < 10) {
+        document.getElementById("mensagem2").innerHTML = "Nome de usuário deve possuir no mínimo 10 caracteres."
+        document.getElementById('cadastro').disabled = true;
+    } else {
+        document.getElementById("mensagem2").innerHTML = ""
+        document.getElementById('cadastro').disabled = false;
+    }
+}
+
+        function validaCPF() {
+            var cpf = document.getElementById("cpf").value.length;
+            if (cpf < 14) {
+                document.getElementById("mensagem3").innerHTML = "O CPF está errado."
+                document.getElementById('cadastro').disabled = true;
+            } else {
+                document.getElementById("mensagem3").innerHTML = ""
+                document.getElementById('cadastro').disabled = false;
+            }
+        }
+
+function verificacheckbox() {
+    var check = document.getElementById("nenhum");
+    var check1 = document.getElementById("meiaPassagem");
+    var check2 = document.getElementById("isencao");
+    
+    if (check.checked && check1.checked  && check2.checked == true) {
+        document.getElementById("mensagem4").innerHTML = ""
+        document.getElementById('cadastro').disabled = false;
+    } else {
+        document.getElementById("mensagem4").innerHTML = "Marque um tipo de benefício que se encaixa!"
+        document.getElementById('cadastro').disabled = true;
+    }
+}
