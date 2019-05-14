05b01bd29163218a52667c5df41052268512d3db
Leonardo Severo <leopedroso45@gmail.com>
2019-05-14 14:31:53 -0300

    Mudando os getters and setters

--- application/models/funcionario.php
+++ application/models/funcionario.php
@@ -21,131 +21,131 @@ class Application_Model_Funcionario extends Zend_Db_Table_Row_Abstract
     //private $bairro;
 
 	public function getId_Usuario(){
-		return this.$id_usuario;
+		return $this->$id_usuario;
 	}
 
-	public function setId_Usuario(){
-		this.$id_usuario = $id_usuario;
+	public function setId_Usuario($id_usuario){
+		$this->$id_usuario = $id_usuario;
 	}
 
 	public function getnome() {
-		return this.$nome;
+		return $this->nome;
 	}
 
 	public function setNome($nome) {
-		this.$nome = $nome;
+		$this->$nome = $nome;
 	}
 
 	public function getDataN() {
-		return this.$dataN;
+		return $this->$dataN;
 	}
 
 	public function setDataN($dataN) {
-		this.$dataN = $dataN;
+		$this->$dataN = $dataN;
 	}
 
 	public function getCpf() {
-		return this.$cpf;
+		return $this->$cpf;
 	}
 
 	public function setCpf($cpf) {
-		this.$cpf = $cpf;
+		$this->$cpf = $cpf;
 	}
 
 	public function getRg() {
-		return this.$rg;
+		return $this->$rg;
 	}
 
 	public function setRg($rg) {
-		this.$rg = $rg;
+		$this->$rg = $rg;
 	}
 
 	public function getCtps() {
-		return this.$ctps;
+		return $this->$ctps;
 	}
 
 	public function setCtps($ctps) {
-		this.$ctps = $ctps;
+		$this->$ctps = $ctps;
 	}
 
 	public function getPis() {
-		return this.$pis;
+		return $this->$pis;
 	}
 
 	public function setPis($pis) {
-		this.$pis = $pis;
+		$this->$pis = $pis;
 	}
 
 	public function getCnh() {
-		return this.$cnh;
+		return $this->$cnh;
 	}
 
 	public function setCnh($cnh) {
-		this.$cnh = $cnh;
+		$this->$cnh = $cnh;
 	}
 
 	public function getTelefone() {
-		return this.$telefone;
+		return $this->$telefone;
 	}
 
 	public function setTelefone($telefone) {
-		this.$telefone = $telefone;
+		$this->$telefone = $telefone;
 	}
 
 	public function getSalario() {
-		return this.$salario;
+		return $this->$salario;
 	}
 
 	public function setSalario($salario) {
-		this.$salario = $salario;
+		$this->$salario = $salario;
 	}
 
 	public function getEmail() {
-		return this.$email;
+		return $this->$email;
 	}
 
 	public function setEmail($email) {
-		this.$email = $email;
+		$this->$email = $email;
 	}
 
 	public function getDataAdmissao() {
-		return this.$dataAdmissao;
+		return $this->$dataAdmissao;
 	}
 
 	public function setDataAdmissao($dataAdmissao) {
-		this.$dataAdmissao = $dataAdmissao;
+		$this->$dataAdmissao = $dataAdmissao;
 	}
 
 	public function getCidade() {
-		return this.$id_cidade;
+		return $this->$id_cidade;
 	}
 
 	public function setCidade($id_cidade) {
-		this.$id_cidade = $id_cidade;
+		$this->$id_cidade = $id_cidade;
 	}
 
 	public function getEndereco() {
-		return this.$endereco;
+		return $this->$endereco;
 	}
 
 	public function setEndereco($endereco) {
-		this.$endereco = $endereco;
+		$this->$endereco = $endereco;
 	}
 
 	public function getNumCasa() {
-		return this.$numCasa;
+		return $this->$numCasa;
 	}
 
 	public function setNumCasa($numCasa) {
-		this.$numCasa = $numCasa;
+		$this->$numCasa = $numCasa;
 	}
 
 	public function getBairro() {
-		return this.$bairro;
+		return $this->$bairro;
 	}
 
 	public function setBairro($bairro) {
-		this.$bairro = $bairro;
+		$this->$bairro = $bairro;
 	}
 
     
