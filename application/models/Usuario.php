515e1083b83474214f8a58bec8c613118fe79d8a
Mauricio Escobar <mauriciosescobar@gmail.com>
2019-05-14 14:40:16 -0300

    usuario e funcionario ok agora!

--- application/models/Usuario.php
+++ application/models/Usuario.php
@@ -2,40 +2,38 @@
 
 class Application_Model_Usuario extends Zend_Db_Table_Row_Abstract
 {
-    //private $id_usuario;
-    //private $id_perfil;
-    //private $nome_usuario;
-    //private $senha;
+//    private $id_perfil;
+//    private $nome_usuario;
+//    private $senha;
 
-	public function getId_usuario() {
-		return $this->$id_usuario;
-	}
+    function getId_perfil()
+    {
+        return $this->id_perfil;
+    }
 
-	public function setId_usuario($id_usuario){
-		$this->$id_usuario = $id_usuario;
-	}
+    function getNome_usuario()
+    {
+        return $this->nome_usuario;
+    }
 
-	public function getId_perfil() {
-		return $this->$id_perfil;
-	}
+    function getSenha()
+    {
+        return $this->senha;
+    }
 
-	public function setId_perfil($id_perfil) {
-		$this->$id_perfil = $id_perfil;
-	}
+    function setId_perfil($id_perfil)
+    {
+        $this->id_perfil = $id_perfil;
+    }
 
-	public function getNome_usuario() {
-		return $this->$nome_usuario;
-	}
+    function setNome_usuario($nome_usuario)
+    {
+        $this->nome_usuario = $nome_usuario;
+    }
 
-	public function setNome_usuario($nome_usuario) {
-		$this->$nome_usuario = $nome_usuario;
-	}
+    function setSenha($senha)
+    {
+        $this->senha = $senha;
+    }
 
-	public function getSenha() {
-		return $this->$senha;
-	}
-
-	public function setSenha($senha) {
-		$this->$senha = $senha;
-	}
 }
