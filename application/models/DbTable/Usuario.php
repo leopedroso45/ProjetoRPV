6870831d8236ebc95e3be5951076775af4d5d165
Mauricio Escobar <mauriciosescobar@gmail.com>
2019-05-14 16:46:18 -0300

    integrando historia 2 pt3

--- application/models/DbTable/Usuario.php
+++ application/models/DbTable/Usuario.php
@@ -17,46 +17,14 @@ class Application_Model_DbTable_Usuario extends Zend_Db_Table_Abstract {
         return $usuario->save();
     }
 
-//    
-//    public function editarUsuario($id,$dados)
-//    {
-//        $usuario = $this->getUsuarioPorId($id);
-//        /*@var $usuario Application_Model_Usuario*/
-//        $usuario->setLogin($dados['login']);
-//        $usuario->setSenha($dados['senha']);
-//        
-//        return $usuario->save();
-//    }
-//    
-////        public function alterarCaminhoFoto($caminho, $id)
-////    {
-////        $select = $this->select()->where('cod_usuario = ?', $id);
-////        $usuario = $this->fetchRow($select);
-////        /* @var $usuario Application_Model_Usuario */
-////        $usuario->setFoto($caminho);
-////        
-////        return $usuario->save();
-////    }
-//    
-//     public function excluirUsuario($id) 
-//   {
-//        $select = $this->select()->where('cod_usuario = ?',$id);
-//        $usuario =  $this->fetchRow($select);
-//           return $usuario->delete();
-//        
-//   }
-//    
-//     public function listarTodosUsuario(){
-//       return $this->fetchAll();
-//    }
-//    
-//    public function getUsuarioPorId($id){
-//        $select = $this->select()->where('cod_usuario = ?',$id);
-//        return $this->fetchRow($select);
-//    }
-//    
-//    public function getUsuarioPorLogin($login){
-//        $select = $this->select()->where('login = ?',$login);
-//        return $this->fetchRow($select);
-//    }
+    public function cadastrarUsuarioF($dados)
+    {
+        $usuario = $this->createRow();
+        $usuario->setId_perfil($dados['id_perfil']);
+        $usuario->setNome_usuario($dados['nome_usuario']);
+        $usuario->setSenha($dados['senha']);
+        
+        return $usuario->save();
+    } 
+	
 }
