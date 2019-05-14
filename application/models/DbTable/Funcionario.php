3cf04074497f5d34b391d645dd150149ac0ed0aa
Leonardo Severo <leopedroso45@gmail.com>
2019-05-14 15:08:08 -0300

    mais

--- application/models/DbTable/Funcionario.php
+++ application/models/DbTable/Funcionario.php
@@ -30,7 +30,7 @@ class Application_Model_DbTable_Funcionario extends Zend_Db_Table_Abstract
         $funcionario->setNumCasa($dados['numCasa']);
         $funcionario->setBairro($dados['bairro']);
 
-
+        var_dump($funcionario); die;
         return $funcionario->save();
     }
 
