53fdce49a182c06499c45b25a6b70ecd504d575d
mauriciounipampa <mauriciosescobar@gmail.com>
2019-05-20 10:00:11 -0300

    Tarifa

---
+++ application/models/Tarifa.php
@@ -0,0 +1,50 @@
+<?php
+
+class Application_Model_Tarifa extends Zend_Db_Table_Row_Abstract
+{
+    //private $valor;
+    //private $vigencia;
+    //private $situacao;
+    //private $id_trajeto;
+    
+    function getValor()
+    {
+        return $this->valor;
+    }
+
+    function getVigencia()
+    {
+        return $this->vigencia;
+    }
+
+    function getSituacao()
+    {
+        return $this->situacao;
+    }
+
+    function getId_trajeto()
+    {
+        return $this->id_trajeto;
+    }
+
+    function setValor($valor)
+    {
+        $this->valor = $valor;
+    }
+
+    function setVigencia($vigencia)
+    {
+        $this->vigencia = $vigencia;
+    }
+
+    function setSituacao($situacao)
+    {
+        $this->situacao = $situacao;
+    }
+
+    function setId_trajeto($id_trajeto)
+    {
+        $this->id_trajeto = $id_trajeto;
+    }
+
+}
