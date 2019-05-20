0c2e8df4a9e4d8eb875f4f0aebf67f717878daaa
Vagner Quincozes <vequincozes@gmail.com>
2019-05-20 18:47:19 -0300

    listagem de onibus urbanos e interurbanos (tem que verificar se não vai duplicar o id na hora se selecionar um)

--- application/controllers/SinistroController.php
+++ application/controllers/SinistroController.php
@@ -12,7 +12,18 @@ class SinistroController extends Zend_Controller_Action {
         $dbTableSinistro = new Application_Model_DbTable_Sinistro();
         $listarSinistros = $dbTableSinistro->listarSinistros();
         $this->view->listaDeSinistros = $listarSinistros;
-                if ($this->getRequest()->isPost()) {
+
+        $dbTableOnibusIntermunicipal = new Application_Model_DbTable_OnibusIntermunicipal();
+        $lista = $dbTableOnibusIntermunicipal->listarTodosOnibusIntermunicipais();
+        $this->view->listarTodosOnibusIntermunicipais = $lista;
+
+        $dbTableOnibusUrbano = new Application_Model_DbTable_OnibusUrbano();
+        $lista1 = $dbTableOnibusUrbano->listarTodosOnibusUrbanos();
+        $this->view->listaDosOnibusUrbanos = $lista1;
+
+
+
+        if ($this->getRequest()->isPost()) {
             $dados = $this->getRequest()->getParams();
             $dbTableSinistro = new Application_Model_DbTable_Sinistro();
             $dbTableSinistro->cadastrarSinistro($dados);
