91c11c1a31da1d10f3fe4b00c533c51287c593e7
Vagner Quincozes <vequincozes@gmail.com>
2019-05-22 21:44:49 -0300

    Começando validar campos

--- public/dist/js/sinistro.js
+++ public/dist/js/sinistro.js
@@ -1,6 +1,22 @@
 $(document).ready(function () {
 
 
+    $("#descricao").blur(function ()
+    {
+        var descricao = $("#descricao").val();
+        if (descricao.length === 0) {
+            $('.descricao').removeClass('hidden');
+        }
+    });
+
+    $("#causa").blur(function ()
+    {
+        var causa = $("#causa").val();
+        if (causa.length === 0) {
+            $('.causa').removeClass('hidden');
+        }
+    });
+
     $(".cadastrar").click(function ()
     {
         var descricao = $("#descricao").val();
@@ -11,39 +27,46 @@ $(document).ready(function () {
         var hora = $("#hora").val();
         var pagante = $("#pagante").val();
 
-        $.ajax({
-            type: "POST",
-            url: baseUrl + 'sinistro/index',
-            async: false,
-            data: {descricao: descricao, causa: causa, culpado: culpado, custo: custo, data: data, hora: hora, pagante: pagante
-            },
-            success: function () {
-                var dialog = bootbox.dialog({
-                    title: 'Mensagem',
-                    message: '<p><i class="fa fa-spin fa-spinner"></i> Salvando...</p>',
-                    closeButton: false,
-                    buttons: {
-                        ok: {
-                            label: "OK",
-                            className: 'btn-primary',
-                            callback: function () {
-                                window.location = baseUrl + 'sinistro';
+        if (descricao.length === 0) {
+            $('.descricao').removeClass('hidden');
+        } else if (causa.length === 0) {
+            $('.causa').removeClass('hidden');
+        } else {
+            $.ajax({
+                type: "POST",
+                url: baseUrl + 'sinistro/index',
+                async: false,
+                data: {descricao: descricao, causa: causa, culpado: culpado, custo: custo, data: data, hora: hora, pagante: pagante
+                },
+                success: function () {
+                    var dialog = bootbox.dialog({
+                        title: 'Mensagem',
+                        message: '<p><i class="fa fa-spin fa-spinner"></i> Salvando...</p>',
+                        closeButton: false,
+                        buttons: {
+                            ok: {
+                                label: "OK",
+                                className: 'btn-primary',
+                                callback: function () {
+                                    window.location = baseUrl + 'sinistro';
+                                }
                             }
                         }
-                    }
-                });
-                dialog.init(function () {
-                    setTimeout(function () {
+                    });
+                    dialog.init(function () {
+                        setTimeout(function () {
 
-                        dialog.find('.bootbox-body').html('Sinistro cadastrado com sucesso!');
-                    }, 1000);
-                });
-            },
-            error: function () {
-                alert('error');
-            }
-        });
+                            dialog.find('.bootbox-body').html('Sinistro cadastrado com sucesso!');
+                        }, 1000);
+                    });
+                },
+                error: function () {
+                    alert('error');
+                }
+            });
+        }
     });
+
 });
 
 $("#alocar").click(function () {
