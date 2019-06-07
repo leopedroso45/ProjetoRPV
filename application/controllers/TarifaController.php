404b4bfa462c840cafebedede2b259c0941ebb7b
mauriciounipampa <mauriciosescobar@gmail.com>
2019-06-06 21:05:51 -0300

    tarifa

--- application/controllers/TarifaController.php
+++ application/controllers/TarifaController.php
@@ -13,28 +13,19 @@ class TarifaController extends Zend_Controller_Action
         $dbTableTrajeto = new Application_Model_DbTable_Trajeto();
         $lista = $dbTableTrajeto->listarTrajetos();
         $this->view->listarTrajetos = $lista;
-        
+
         if ($this->getRequest()->isPost()) {
-                $dados = $this->getRequest()->getParams();
-                $trajetos = explode(',', $dados['id_trajeto']);
+            $dados = $this->getRequest()->getParams();
+            $trajetos = explode(',', $dados['id_trajeto']);
+            
+            for ($i = 0; $i < sizeof($trajetos); $i++) {
+                $dbTableTarifa = new Application_Model_DbTable_Tarifa();
+                $id_tarifa = $dbTableTarifa->cadastrarTarifa($dados);
 
-
-
-                for($i = 0; $i < sizeof($trajetos); $i++){   
-  
-                    $dbTableTarifa = new Application_Model_DbTable_Tarifa();
-                    $id_tarifa=$dbTableTarifa->cadastrarTarifa($dados);
-
-                    $dbTableTarifa_Trajeto = new Application_Model_DbTable_Tarifa_Trajeto();
-                    $dbTableTarifa_Trajeto->cadastrarTarifaTrajeto($dados, $id_tarifa, $trajetos[$i]);
-                }
-
+                $dbTableTarifa_Trajeto = new Application_Model_DbTable_Tarifa_Trajeto();
+                $dbTableTarifa_Trajeto->cadastrarTarifaTrajeto($dados, $id_tarifa, $trajetos[$i]);
+            }
         }
-    }
-
-    public function cadastrarAction()
-    {
-   
     }
 
 }
--- application/models/DbTable/tarifa_trajeto.php
+++ application/models/DbTable/tarifa_trajeto.php
@@ -7,6 +7,7 @@ class Application_Model_DbTable_Tarifa_Trajeto extends Zend_Db_Table_Abstract
     public function cadastrarTarifaTrajeto($dados, $id_tarifa, $trajeto)
     {
         $tarifa_trajeto = $this->createRow();
+        /*@var $tarifa_trajeto Application_Model_Tarifa_Trajeto*/
         $tarifa_trajeto->setId_tarifa($id_tarifa);
         $tarifa_trajeto->setId_trajeto($trajeto);
         $tarifa_trajeto->setValor($dados['valor']);
